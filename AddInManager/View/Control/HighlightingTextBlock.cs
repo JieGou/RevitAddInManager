2c8eee1494644423329460a030eb25e7d6eab3a8
陈杰 <jiegou04@gmail.com>
2022-03-11 21:19:16 +0800

    完成搜索高亮——使用后台代码方式

---
+++ AddInManager/View/Control/HighlightingTextBlock.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace RevitAddinManager.View.Control
+{
+  /// <summary>
+  /// 带高亮的文本框<see cref="TextBlock"/>
+  /// </summary>
+  [TemplatePart(Name = HighlighttextblockName, Type = typeof(TextBlock))]
+  public class HighlightingTextBlock : System.Windows.Controls.Control
+  {
+    private const string HighlighttextblockName = "PART_HighlightTextblock";
+
+    private static readonly DependencyPropertyKey MatchCountPropertyKey
+        = DependencyProperty.RegisterReadOnly("MatchCount",
+                                              typeof(int),
+                                              typeof(HighlightingTextBlock),
+                                              new PropertyMetadata(0));
+
+    public static readonly DependencyProperty MatchCountProperty
+        = MatchCountPropertyKey.DependencyProperty;
+
+    /// <summary>
+    /// 高亮的文本依赖属性
+    /// </summary>
+    public static readonly DependencyProperty HighlightTextProperty
+      = DependencyProperty.Register("HighlightText",
+                                    typeof(string),
+                                    typeof(HighlightingTextBlock),
+                                    new PropertyMetadata(string.Empty, OnHighlightTextPropertyChanged));
+
+    /// <summary>
+    /// 文本依赖属性
+    /// </summary>
+    public static readonly DependencyProperty TextProperty
+      = TextBlock.TextProperty.AddOwner(typeof(HighlightingTextBlock),
+                                        new PropertyMetadata(string.Empty, OnTextPropertyChanged));
+
+    /// <summary>
+    /// 折行依赖属性
+    /// </summary>
+    public static readonly DependencyProperty TextWrappingProperty
+      = TextBlock.TextWrappingProperty.AddOwner(typeof(HighlightingTextBlock),
+                                                new PropertyMetadata(TextWrapping.NoWrap));
+
+    /// <summary>
+    /// 修整文本依赖属性
+    /// </summary>
+    public static readonly DependencyProperty TextTrimmingProperty
+      = TextBlock.TextTrimmingProperty.AddOwner(typeof(HighlightingTextBlock),
+                                                new PropertyMetadata(TextTrimming.None));
+
+    /// <summary>
+    /// 高亮前景颜色依赖属性
+    /// </summary>
+    public static readonly DependencyProperty HighlightForegroundProperty
+      = DependencyProperty.Register("HighlightForeground",
+                                    typeof(Brush),
+                                    typeof(HighlightingTextBlock),
+                                    new PropertyMetadata(Brushes.White));
+
+    /// <summary>
+    /// 高亮<see cref="FontStyle"/>依赖属性
+    /// </summary>
+    public static readonly DependencyProperty HighlightFontStyleProperty
+      = DependencyProperty.Register("HighlightFontStyle",
+                                    typeof(FontStyle),
+                                    typeof(HighlightingTextBlock),
+                                    new PropertyMetadata(FontStyles.Normal));
+
+    /// <summary>
+    /// 高亮<see cref="FontWeight"/>依赖属性
+    /// </summary>
+    public static readonly DependencyProperty HighlightFontWeightProperty
+      = DependencyProperty.Register("HighlightFontWeight",
+                                    typeof(FontWeight),
+                                    typeof(HighlightingTextBlock),
+                                    new PropertyMetadata(FontWeights.Normal));
+
+    /// <summary>
+    /// 高亮文本修饰<see cref="TextDecorations"/>依赖属性
+    /// </summary>
+    public static readonly DependencyProperty HighlightTextDecorationsProperty
+      = DependencyProperty.Register("HighlightTextDecorations",
+                                    typeof(TextDecorationCollection),
+                                    typeof(HighlightingTextBlock),
+                                    new PropertyMetadata(TextDecorations.Underline));
+
+    /// <summary>
+    /// 高亮背景颜色依赖属性
+    /// </summary>
+    public static readonly DependencyProperty HighlightBackgroundProperty
+      = DependencyProperty.Register("HighlightBackground",
+                                    typeof(Brush),
+                                    typeof(HighlightingTextBlock),
+                                    new PropertyMetadata(Brushes.Blue));
+
+    //Done 高亮部分匹配比较 是否忽略大小写
+    /// <summary>
+    /// 高亮文本匹配规则
+    /// </summary>
+    public static readonly DependencyProperty HighlightStringComparisonProperty
+      = DependencyProperty.Register("HighlightStringComparison",
+                                    typeof(StringComparison),
+                                    typeof(HighlightingTextBlock),
+                                    new PropertyMetadata(StringComparison.InvariantCultureIgnoreCase));
+
+    /// <summary>
+    /// 文本框
+    /// </summary>
+    private TextBlock highlightTextBlock;
+
+    static HighlightingTextBlock()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(HighlightingTextBlock),
+          new FrameworkPropertyMetadata(typeof(HighlightingTextBlock)));
+    }
+
+    /// <summary>
+    /// 匹配数量
+    /// </summary>
+    public int MatchCount
+    {
+      get => (int)GetValue(MatchCountProperty);
+      protected set => SetValue(MatchCountPropertyKey, value);
+    }
+
+    /// <summary>
+    /// 高亮背景颜色
+    /// </summary>
+    public Brush HighlightBackground
+    {
+      get => (Brush)GetValue(HighlightBackgroundProperty);
+      set => SetValue(HighlightBackgroundProperty, value);
+    }
+
+    /// <summary>
+    /// 高亮文本匹配规则
+    /// </summary>
+    public StringComparison HighlightStringComparison
+    {
+      get => (StringComparison)GetValue(HighlightStringComparisonProperty);
+      set => SetValue(HighlightStringComparisonProperty, value);
+    }
+
+    /// <summary>
+    /// 高亮前景颜色
+    /// </summary>
+    public Brush HighlightForeground
+    {
+      get => (Brush)GetValue(HighlightForegroundProperty);
+      set => SetValue(HighlightForegroundProperty, value);
+    }
+
+    /// <summary>
+    /// 高亮<see cref="FontStyle"/>
+    /// </summary>
+    public FontStyle HighlightFontStyle
+    {
+      get => (FontStyle)GetValue(HighlightFontStyleProperty);
+      set => SetValue(HighlightFontStyleProperty, value);
+    }
+
+    /// <summary>
+    /// 高亮<see cref="FontWeight"/>
+    /// </summary>
+    public FontWeight HighlightFontWeight
+    {
+      get => (FontWeight)GetValue(HighlightFontWeightProperty);
+      set => SetValue(HighlightFontWeightProperty, value);
+    }
+
+    /// <summary>
+    /// 高亮文本修饰<see cref="TextDecorations"/>
+    /// </summary>
+    public TextDecorationCollection HighlightTextDecorations
+    {
+      get => (TextDecorationCollection)GetValue(HighlightTextDecorationsProperty);
+      set => SetValue(HighlightTextDecorationsProperty, value);
+    }
+
+    /// <summary>
+    /// 高亮的文本
+    /// </summary>
+    public string HighlightText
+    {
+      get => (string)GetValue(HighlightTextProperty);
+      set => SetValue(HighlightTextProperty, value);
+    }
+
+    /// <summary>
+    /// 文本
+    /// </summary>
+    public string Text
+    {
+      get => (string)GetValue(TextProperty);
+      set => SetValue(TextProperty, value);
+    }
+
+    /// <summary>
+    /// 折行
+    /// </summary>
+    public TextWrapping TextWrapping
+    {
+      get => (TextWrapping)GetValue(TextWrappingProperty);
+      set => SetValue(TextWrappingProperty, value);
+    }
+
+    /// <summary>
+    /// 修整文本
+    /// </summary>
+    public TextTrimming TextTrimming
+    {
+      get => (TextTrimming)GetValue(TextTrimmingProperty);
+      set => SetValue(TextTrimmingProperty, value);
+    }
+
+    /// <summary>
+    /// <see cref="HighlightText"/>属性改变时回调函数
+    /// </summary>
+    /// <param name="d"></param>
+    /// <param name="e"></param>
+    private static void OnHighlightTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      var textblock = (HighlightingTextBlock)d;
+      textblock.ProcessTextChanged(textblock.Text, e.NewValue as string);
+    }
+
+    private static void OnTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      var textblock = (HighlightingTextBlock)d;
+      textblock.ProcessTextChanged(e.NewValue as string, textblock.HighlightText);
+    }
+
+    /// <summary>
+    /// 文本改变时的处理函数
+    /// </summary>
+    /// <param name="mainText"></param>
+    /// <param name="highlightText"></param>
+    private void ProcessTextChanged(string mainText, string highlightText)
+    {
+      if (highlightTextBlock == null)
+      {
+        return;
+      }
+
+      highlightTextBlock.Inlines.Clear();
+      SetValue(MatchCountPropertyKey, 0);
+
+      if (highlightTextBlock == null || string.IsNullOrWhiteSpace(mainText))
+      {
+        return;
+      }
+
+      if (string.IsNullOrWhiteSpace(highlightText))
+      {
+        var completeRun = new Run(mainText);
+        highlightTextBlock.Inlines.Add(completeRun);
+        return;
+      }
+
+      if (highlightText.Contains(" ") || highlightText.Contains(","))
+      {
+        var textArray = highlightText.Split(new string[] { " ", "," },
+                                            StringSplitOptions.RemoveEmptyEntries);
+        ColorBrushTextParts(mainText, textArray.ToList());
+      }
+      else
+      {
+        ColorBrushTextParts(mainText, new List<string>() { highlightText });
+      }
+    }
+
+    /// <summary>
+    /// 对匹配的文字着色
+    /// </summary>
+    /// <param name="mainText">文字</param>
+    /// <param name="highlightText">匹配的需要亮显的文字</param>
+    private void ColorBrushTextPart(string mainText, string highlightText)
+    {
+      var find = 0;
+      var searchTextLength = highlightText.Length;
+      while (true)
+      {
+        var oldFind = find;
+        find = mainText.IndexOf(highlightText, find, HighlightStringComparison);
+        if (find == -1)
+        {
+          highlightTextBlock.Inlines.Add(
+              oldFind > 0
+                  ? GetRunForText(mainText.Substring(oldFind, mainText.Length - oldFind), false)
+                  : GetRunForText(mainText, false));
+          break;
+        }
+
+        if (oldFind == find)
+        {
+          highlightTextBlock.Inlines.Add(GetRunForText(mainText.Substring(oldFind, searchTextLength), true));
+          SetValue(MatchCountPropertyKey, MatchCount + 1);
+          find = find + searchTextLength;
+          continue;
+        }
+
+        highlightTextBlock.Inlines.Add(GetRunForText(mainText.Substring(oldFind, find - oldFind), false));
+      }
+    }
+
+    /// <summary>
+    /// 对匹配的文字列表着色 即多个条件
+    /// </summary>
+    /// <param name="mainText">文字</param>
+    /// <param name="highlightTextList">匹配的需要亮显的文字</param>
+    private void ColorBrushTextParts(string mainText, List<string> highlightTextList)
+    {
+      if (highlightTextList.Count == 1)
+      {
+        ColorBrushTextPart(mainText, highlightTextList[0]);
+        return;
+      }
+
+      var find = 0;
+      while (true)
+      {
+        var oldFind = find;
+
+        //能匹配的 需要亮显的字符串
+        var canBeMatchTextList =
+            (from item in highlightTextList
+             let index = mainText.IndexOf(item, find, HighlightStringComparison)
+             where index >= 0
+             orderby index
+             select item)
+            .ToList();
+
+        if (canBeMatchTextList.Count == 0)
+        {
+          //字符串的最后不着色
+          highlightTextBlock.Inlines.Add(GetRunForText(mainText.Substring(oldFind, mainText.Length - oldFind), false));
+          break;
+        }
+
+        string highlightText = canBeMatchTextList.First();
+        var searchTextLength = highlightText.Length;
+
+        find = mainText.IndexOf(highlightText, find, HighlightStringComparison);
+        if (find == -1)
+        {
+          highlightTextBlock.Inlines.Add(
+              oldFind > 0
+                  ? GetRunForText(mainText.Substring(oldFind, mainText.Length - oldFind), false)
+                  : GetRunForText(mainText, false));
+          break;
+        }
+
+        if (oldFind == find)
+        {
+          highlightTextBlock.Inlines.Add(GetRunForText(mainText.Substring(oldFind, searchTextLength), true));
+          SetValue(MatchCountPropertyKey, MatchCount + 1);
+          find = find + searchTextLength;
+          continue;
+        }
+
+        highlightTextBlock.Inlines.Add(GetRunForText(mainText.Substring(oldFind, find - oldFind), false));
+      }
+    }
+
+    /// <summary>
+    /// 为匹配的文字部分添加着色
+    /// </summary>
+    /// <param name="text">匹配的文字部分</param>
+    /// <param name="isHighlighted">是否亮显（着色）</param>
+    /// <returns></returns>
+    private Run GetRunForText(string text, bool isHighlighted)
+    {
+      var textRun = new Run(text)
+      {
+        Foreground = isHighlighted ? HighlightForeground : Foreground,
+        Background = isHighlighted ? HighlightBackground : Background,
+
+        //Done 高亮部分斜体 FontStyle
+        FontStyle = isHighlighted ? HighlightFontStyle : FontStyle,
+
+        //Done 高亮部分加粗 FontWeight="Bold"
+        FontWeight = isHighlighted ? HighlightFontWeight : FontWeight,
+
+        //Done 高亮部分添加下划线
+        TextDecorations = isHighlighted ? HighlightTextDecorations : highlightTextBlock.TextDecorations,
+      };
+
+      return textRun;
+    }
+
+    public override void OnApplyTemplate()
+    {
+      highlightTextBlock = GetTemplateChild(HighlighttextblockName) as TextBlock;
+      if (highlightTextBlock == null)
+      {
+        return;
+      }
+
+      ProcessTextChanged(Text, HighlightText);
+    }
+  }
+
+}
--- AddInManager/View/FrmAddInManager.xaml.cs
+++ AddInManager/View/FrmAddInManager.xaml.cs
@@ -1,8 +1,17 @@
 ﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
 using RevitAddinManager.Model;
 using RevitAddinManager.ViewModel;
+
+//Done 1、搜索高亮
+//TODO 2、右键切换选择项
+//TODO 3、图标以及快捷键
 
 namespace RevitAddinManager.View;
 
@@ -22,16 +31,136 @@ public partial class FrmAddInManager : Window
 
     private void TbxDescription_OnLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
     {
-        if (viewModel.MAddinManagerBase.ActiveCmdItem != null && TabControl.SelectedIndex==0)
+        if (viewModel.MAddinManagerBase.ActiveCmdItem != null && TabControl.SelectedIndex == 0)
         {
             viewModel.MAddinManagerBase.ActiveCmdItem.Description = TbxDescription.Text;
         }
-        if (viewModel.MAddinManagerBase.ActiveAppItem != null && TabControl.SelectedIndex==1)
+        if (viewModel.MAddinManagerBase.ActiveAppItem != null && TabControl.SelectedIndex == 1)
         {
             viewModel.MAddinManagerBase.ActiveAppItem.Description = TbxDescription.Text;
         }
         viewModel.MAddinManagerBase.AddinManager.SaveToAimIni();
     }
+    DispatcherTimer _typingTimer; // WPF
+    private void tbxSearch_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+    {
+        if (_typingTimer == null)
+        {
+            _typingTimer = new DispatcherTimer();
+            _typingTimer.Interval = TimeSpan.FromMilliseconds(200);
 
-    
+            _typingTimer.Tick += new EventHandler(this.handleTypingTimerTimeout);
+        }
+        _typingTimer.Stop(); // Resets the timer
+        _typingTimer.Tag = (sender as TextBox).Text; // This should be done with EventArgs
+        _typingTimer.Start();
+
+    }
+    private void handleTypingTimerTimeout(object sender, EventArgs e)
+    {
+        //var timer = sender as Timer; // WinForms
+        var timer = sender as DispatcherTimer; // WPF
+        if (timer == null)
+        {
+            return;
+        }
+
+        // Testing - updates window title
+        var isbn = timer.Tag.ToString();
+        //windowFrame.Text = isbn; // WinForms
+        //windowFrame.Title = isbn; // WPF
+        FindControlItem(this.TreeViewCommand, isbn);
+        FindControlItem(this.TreeViewApp, isbn);
+        FindControlItem(this.DataGridStartup, isbn);
+        // The timer must be stopped! We want to act only once per keystroke.
+        timer.Stop();
+    }
+
+    public void FindControlItem(DependencyObject obj, string searchStr)
+    {
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+        {
+            ListViewItem lv = obj as ListViewItem;
+            DataGridCell dg = obj as DataGridCell;
+            TreeViewItem tv = obj as TreeViewItem;
+            if (lv != null)
+            {
+                HighlightText(lv, tbxSearch.Text, isBold: true);
+            }
+            if (dg != null)
+            {
+                HighlightText(dg, tbxSearch.Text, isBold: true);
+            }
+            if (tv != null)
+            {
+                HighlightText(tv, tbxSearch.Text, isBold: true);
+            }
+            FindControlItem(VisualTreeHelper.GetChild(obj as DependencyObject, i), searchStr);
+        }
+    }
+    //From http://u6.gg/ksn8p 短地址
+    /// <summary>
+    /// 高亮文本
+    /// </summary>
+    /// <param name="itx">包含文本的控件(容器)</param>
+    /// <param name="searchKeywords">搜索关键字</param>
+    /// <param name="background">背景色 默认黄色</param>
+    /// <param name="foreground">前景色 默认黑色</param>
+    /// <param name="isItalic">是否高亮部分斜体 默认true</param>
+    /// <param name="isUnderline">是否高亮部分下划线 默认true</param>
+    /// <param name="isBold">是否高亮部分加粗 默认false</param>
+    private void HighlightText(Object itx, string searchKeywords,
+                               Brush background = default(Brush),
+                               Brush foreground = default(Brush),
+                               bool isItalic = true,
+                               bool isUnderline = true,
+                               bool isBold = false)
+    {
+        if (background == default(Brush)) { background = Brushes.Yellow; }
+        if (foreground == default(Brush)) { foreground = Brushes.Black; }
+
+        if (itx == null) return;
+        if (itx is TextBlock)
+        {
+            var regex = new Regex("(" + searchKeywords + ")", RegexOptions.IgnoreCase);
+            TextBlock tb = itx as TextBlock;
+            if (searchKeywords.Length == 0)
+            {
+                string str = tb.Text;
+                tb.Inlines.Clear();
+                tb.Inlines.Add(str);
+                return;
+            }
+            string[] substrings = regex.Split(tb.Text);
+            tb.Inlines.Clear();
+            foreach (var item in substrings)
+            {
+                if (regex.Match(item).Success)
+                {
+                    Run runx = new Run(item);
+                    runx.Background = background;
+                    runx.Foreground = foreground;
+                    if (isItalic) runx.FontStyle = FontStyles.Italic;
+                    if (isBold) runx.FontWeight = FontWeights.Bold;
+                    if (isUnderline) runx.TextDecorations = new TextDecorationCollection()
+                        {
+                            new TextDecoration() { Pen= new Pen(Brushes.Blue,1), PenThicknessUnit=TextDecorationUnit.FontRecommended, },
+                        };
+                    tb.Inlines.Add(runx);
+                }
+                else
+                {
+                    tb.Inlines.Add(item);
+                }
+            }
+            return;
+        }
+        else
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(itx as DependencyObject); i++)
+            {
+                HighlightText(VisualTreeHelper.GetChild(itx as DependencyObject, i), searchKeywords, background, foreground, isItalic, isUnderline, isBold);
+            }
+        }
+    }
 }
